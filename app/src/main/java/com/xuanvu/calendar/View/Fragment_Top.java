9b9f10187058faae0b70eae5b78fead4f0f07717
Phan Xuân Vũ <37499858+VuPhan99@users.noreply.github.com>
2019-06-11 13:48:53 +0700

    update top detail

--- app/src/main/java/com/xuanvu/calendar/View/Fragment_Top.java
+++ app/src/main/java/com/xuanvu/calendar/View/Fragment_Top.java
@@ -8,7 +8,9 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.LinearLayout;
 import android.widget.TextView;
+import android.widget.ToggleButton;
 
 import com.xuanvu.calendar.R;
 
@@ -16,31 +18,53 @@ import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Date;
 
-import butterknife.BindView;
-import butterknife.ButterKnife;
-import butterknife.Unbinder;
-
 public class Fragment_Top extends Fragment {
 
     private TextView tv_date;
-
-    Unbinder unbinder;
-
-    @BindView( R.id.btn_sheetDialog )
-    Button btn_sheetDialog;
+    private ToggleButton toggleButton;
+    private LinearLayout layout_detail_view;
+    private Button btn_change_day, btn_setting;
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         super.onCreateView( inflater, container, savedInstanceState );
-        View view = inflater.inflate( R.layout.fragment_top,container,false );
+        View view = inflater.inflate( R.layout.fragment_top, container, false );
+
+        LinearLayout llBottomSheet = view.findViewById( R.id.bottom_sheet_top );
         TextView tv_date = view.findViewById( R.id.tv_date );
+
+        layout_detail_view = view.findViewById( R.id.layout_detail_view );
+
+        layout_detail_view.setVisibility( View.GONE );
+
+        btn_change_day = view.findViewById( R.id.btn_change_day );
+        btn_setting = view.findViewById( R.id.btn_setting );
+
+        btn_change_day.setVisibility( View.GONE );
+        btn_setting.setVisibility( View.GONE );
+
+        Button btn_top_detail = view.findViewById( R.id.btn_top_detail );
+        btn_top_detail.setOnClickListener( new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (((ToggleButton) v).isChecked()) {
+                    layout_detail_view.setVisibility( View.VISIBLE );
+                    btn_change_day.setVisibility( View.VISIBLE );
+                    btn_setting.setVisibility( View.VISIBLE );
+                } else {
+                    layout_detail_view.setVisibility( View.GONE );
+                    btn_change_day.setVisibility( View.GONE );
+                    btn_setting.setVisibility( View.GONE );
+                }
+            }
+        } );
+
         Calendar calendar = Calendar.getInstance();
         SimpleDateFormat sdf = new SimpleDateFormat( "EEE dd MMMM yyyy" );/*EEE MMM d yyyy*/
         String currentDateandTime = sdf.format( new Date() );
         tv_date.setText( currentDateandTime );
 
-        unbinder = ButterKnife.bind(this, view);
         return view;
     }
 
--- app/src/main/java/com/xuanvu/calendar/View/MainActivity.java
+++ app/src/main/java/com/xuanvu/calendar/View/MainActivity.java
@@ -1,27 +1,15 @@
 package com.xuanvu.calendar.View;
 
 import android.os.Bundle;
-import android.support.annotation.NonNull;
-import android.support.design.widget.BottomSheetBehavior;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 import android.view.View;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.widget.Button;
-import android.widget.CalendarView;
-import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import com.xuanvu.calendar.R;
 
-import java.text.SimpleDateFormat;
-import java.util.Calendar;
-import java.util.Date;
-
-import butterknife.BindView;
 import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
@@ -29,34 +17,34 @@ public class MainActivity extends AppCompatActivity {
     private TextView tv_date;
     private Button btn_sheetDialogl;
 
-    @BindView( R.id.btnBottomSheet )
+/*    @BindView( R.id.btnBottomSheet )
     Button btnBottomSheet;
 
     @BindView(R.id.bottom_sheet_top )
     LinearLayout bottomSheetLayout;
-    BottomSheetBehavior sheetBehavior;
+    BottomSheetBehavior sheetBehavior;*/
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate( savedInstanceState );
         setContentView( R.layout.activity_main );
-        ButterKnife.bind(MainActivity.this);
-       /* Toolbar toolbar = findViewById( R.id.toolbar );
-*/
+        ButterKnife.bind( MainActivity.this );
+        /* Toolbar toolbar = findViewById( R.id.toolbar );
+         */
         /*TextView tv_date = findViewById( R.id.tv_date );
         Calendar calendar = Calendar.getInstance();
         SimpleDateFormat sdf = new SimpleDateFormat("EEE dd MMMM yyyy");*//*EEE MMM d yyyy*//*
         String currentDateandTime = sdf.format(new Date());
         tv_date.setText(currentDateandTime);*/
-  /*      tv_date.append( calendar.getTime()+"" );*//*
+        /*      tv_date.append( calendar.getTime()+"" );*//*
         tv_date.append( calendar.get( Calendar.DATE ) + "" );
         tv_date.append( calendar.get( Calendar.MONTH ) + "" );
         tv_date.append( calendar.get( Calendar.DAY_OF_WEEK_IN_MONTH )+"" );
         tv_date.append( calendar.get( Calendar.MONTH )+"" );*//*
-*/
+         */
         /*setSupportActionBar( toolbar );
-*/
+         */
         FloatingActionButton fab = findViewById( R.id.fab );
         fab.setOnClickListener( new View.OnClickListener() {
             @Override
@@ -65,38 +53,6 @@ public class MainActivity extends AppCompatActivity {
             }
         } );
 
-
-
-        //init bottomShet
-        sheetBehavior = BottomSheetBehavior.from(bottomSheetLayout);
-
-        //Callback function of bottom sheet
-        sheetBehavior.setBottomSheetCallback( new BottomSheetBehavior.BottomSheetCallback() {
-            @Override
-            public void onStateChanged(@NonNull View view, int i) {
-                switch (i) {
-                    case BottomSheetBehavior.STATE_HIDDEN:
-                        break;
-                    case BottomSheetBehavior.STATE_EXPANDED: {
-                        btnBottomSheet.setText("Close Sheet");
-                    }
-                    break;
-                    case BottomSheetBehavior.STATE_COLLAPSED: {
-                        btnBottomSheet.setText("Expand Sheet");
-                    }
-                    break;
-                    case BottomSheetBehavior.STATE_DRAGGING:
-                        break;
-                    case BottomSheetBehavior.STATE_SETTLING:
-                        break;
-                }
-            }
-
-            @Override
-            public void onSlide(@NonNull View view, float v) {
-
-            }
-        } );
 
     }
 
